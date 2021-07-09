266d3e67952a5e70ed3bc2dbd3d09c6e988b657f
shm-318 <shivam318@gmail.com>
2021-07-09 23:14:32 +0530

    production error fixed

--- storekeeper/settings.py
+++ storekeeper/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[STATIC_DIR,]
-#STATIC_ROOT=STATIC_DIR
+#STATICFILES_DIRS=[STATIC_DIR,]
+STATIC_ROOT=STATIC_DIR
 #print(STATICFILES_DIR)
 
 MEDIA_URL='/media/'
